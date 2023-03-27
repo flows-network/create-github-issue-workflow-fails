3283ad829cc3cecf50ce4c1a41ccf10047642fd2
Ji Chen <jaykchen@gmail.com>
2023-03-27 16:45:01 -0400

    clean

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ use flowsnet_platform_sdk::write_error_log;
 use github_flows::{
     get_octo, listen_to_event,
     octocrab::models::events::payload::WorkflowRunEventAction,
-    EventPayload::{self, WorkflowRunEvent},
+    EventPayload,
 };
-use slack_flows::send_message_to_channel;
+
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
@@ -26,24 +26,17 @@ async fn handler(login: &str, owner: &str, repo: &str, payload: EventPayload) {
         EventPayload::WorkflowRunEvent(e) => {
             if e.action == WorkflowRunEventAction::Completed {
                 let workflow_run = e.workflow_run;
-                let conclusion = workflow_run.conclusion.unwrap();
+                let conclusion = workflow_run.conclusion.unwrap_or("".to_string());
 
                 if conclusion != String::from("success") {
-                    let title = workflow_run.name;
+                    let name = workflow_run.name;
                     let run_number = workflow_run.run_number;
-                    let html_url = workflow_run.html_url;
-                    let msg = format!(
-                        "Workflow: {title} #{run_number}\n Status: {conclusion} \n{html_url} \n"
-                    );
-                    send_message_to_channel("ik8", "ch_in", html_url.to_string());
+                    let title = format!("{conclusion} executing {name} run #{run_number}");
 
-                    match issues.create(msg).send().await {
-                        Ok(comment) => {
-                            send_message_to_channel("ik8", "ch_out", comment.body_text.unwrap());
-                        }
+                    match issues.create(title).send().await {
+                        Ok(_) => {}
                         Err(e) => {
                             write_error_log!(e.to_string());
-                            send_message_to_channel("ik8", "ch_err", e.to_string());
                         }
                     };
                 } else {
