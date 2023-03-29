a475574f1e97cbe1ef35593d5a041aa568089040
Ji Chen <jaykchen@gmail.com>
2023-03-29 10:17:23 -0400

    issue body shows the action link

--- src/lib.rs
+++ src/lib.rs
@@ -27,13 +27,23 @@ async fn handler(login: &str, owner: &str, repo: &str, payload: EventPayload) {
                 if conclusion != String::from("success") {
                     let name = workflow_run.name;
                     let run_number = workflow_run.run_number;
+
                     let title = format!("{conclusion} executing {name} run #{run_number}");
+                    let body = workflow_run.html_url.to_string();
 
                     let octo = get_octo(Some(String::from(login)));
                     let issues = octo.issues(owner, repo);
 
                     match issues.create(title).send().await {
-                        Ok(_) => {}
+                        Ok(issue) => {
+                            let issue_number = issue.number;
+                            match issues.create_comment(issue_number, body).await {
+                                Ok(_comment) => {}
+                                Err(e) => {
+                                    write_error_log!(e.to_string());
+                                }
+                            }
+                        }
                         Err(e) => {
                             write_error_log!(e.to_string());
                         }
